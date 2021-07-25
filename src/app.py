5660d6ed6249bbdb192587f595c20d6dc687a4f2
vaggy <sarvagya6@gmail.com>
2021-07-25 20:24:10 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,21 +4,23 @@ from urllib.parse import urlencode
 from .config import app_config
 from pymemcache.client import base
 
-import threading
+#import threading
 
 
 def create_app(env_name):
     app = Flask(__name__)
-    client = base.Client(('localhost', 6969))
+    client = base.Client(('127.0.0.1', 6969))
 
     app.config.from_object(app_config[env_name])
     
     @app.route('/', methods=['GET'])
     def index():
-        return jsonify( message="ok")
-    
-    @app.route('/main/<length>')
-    def main(length):
+        return jsonify(message="nah")
+
+    @app.route('/setup')
+    def main():
+        length=1024
+        global res
         res=[]
         type="uint16"
         endpoint = "https://qrng.anu.edu.au/API/jsonI.php"
@@ -26,13 +28,11 @@ def create_app(env_name):
         lookup_url = f"{endpoint}?{query_params}"
         r = requests.get(lookup_url)
         if r.status_code in range(200,299):
-            if res:
+            if res: #obsolete
                 for i in range(len):
                     res.apprend(r.json()['data'][i])
-                    client.set('cached_res', res)
                     return jsonify(message="updated", hehe="https://save418.com/")
-
-            client.set('cached_res', r.json()['data'])
+            res=r.json()['data']
             return jsonify(message="ready", hehe="https://save418.com/")
     
     def update_response(res,len):
@@ -44,41 +44,30 @@ def create_app(env_name):
         if r.status_code in range(200,299):
             if res:
                 for i in range(len):
-                    res.apprend(r.json()['data'][i])
+                    res.append(r.json()['data'][i])
                     client.set('cached_res', res)
                     return jsonify(message="updated", hehe="https://save418.com/")
 
-        if not res:
-            main(res,len)
+    @app.route('/result')
+    def result():
+        n = request.args['num']
+        n = int(n)
+        print(n)
 
-    app.route('/result')
-    def result(n):
-        n=request.args.get('n')
-
-        res = client.get('cached_res')
-
-        if n==0 or res is None:
-            init_len = 1024
-            res_list=[]
-            main(res_list,init_len)
+        if n==0 or n is None or not res:
+            main()
             if n==0:
                 return jsonify(message="ready", hehe="https://save418.com/")
-            if res is None:
+            if not res:
                 return jsonify(message="created new cache memory. initially not present", hehe="https://save418.com/")
         
-        t = threading.Thread(target=update_response(), args=[res, n])
-        t.setDaemon(False)
-        t.start()
+        #t = threading.Thread(target=update_response(res, n))
+        #t.setDaemon(False)
+        #t.start()
         res_string = ""
         for i in range(n):
             res_string+=str(res.pop())
         
-        return jsonify(res_str = res_string, message="cool")
+        return jsonify(res_str = res_string, message="cool", hehe="https://save418.com/")
 
-
-
-
-            
-
-    
     return app
